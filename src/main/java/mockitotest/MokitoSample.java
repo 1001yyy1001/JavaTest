c7a2d2b369f0851cb093fd248fff935da7814201
suzukiyota <169060298+1001yyy1001@users.noreply.github.com>
2024-05-25 18:42:24 +0900

    質問用

--- src/main/java/mockitotest/MokitoSample.java
+++ src/main/java/mockitotest/MokitoSample.java
@@ -1,5 +1,26 @@
 package mockitotest;
 
-public class MokitoSample {
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.*;
+import static org.mockito.Mockito.*;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class MockitoSampleTest {
+
+    // フィールドは省略
+
+ @Test
+ @DisplayName("getSquareResultに5を渡したときに25が表示できる")
+ void test() {
+  when(calc.square(5)).thenReturn(25);
+  String result = sample.getSquareResult(5);
+
+  // 3. 実行結果の実測値と期待値の検証（アサーション）を行う
+  assertEquals(result, "5の二乗の結果は25");
+ }
 }
